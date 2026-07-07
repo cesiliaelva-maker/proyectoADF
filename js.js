7553bd2a951f7f769a87b072296fd34586f82675
Cesilia <cesiliaelva@gmail.com git config --global user.name Cesilia git commit -m proyectoADF  git config --global user.email cesiliaelva@gmail.com>
2026-07-07 13:51:27 +0200

    inicio proyectoADF

---
+++ js.js
@@ -0,0 +1,13 @@
+
+
+
+
+function boton() {
+   
+
+    let nombre;
+    nombre = prompt("Por favor ingresa tu nombre"); 
+    alert("Hola, bienvenido a mi página web");
+    document.getElementById("demo").innerHTML = "Hola, bienvenido a mi página web    "  + nombre;
+}
+
